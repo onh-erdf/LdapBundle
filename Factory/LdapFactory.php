1d1afe26afe93adf0ff9321a380527e7a13b7630
alborq <alborq@users.noreply.github.com>
2016-01-13 10:55:01 +0100

    Patch to 2.8-3.0

--- Factory/LdapFactory.php
+++ Factory/LdapFactory.php
@@ -51,6 +51,7 @@ class LdapFactory extends AbstractFactory
         $container
             ->setDefinition($dao, new DefinitionDecorator('security.authentication.provider.dao'))
             ->replaceArgument(0, new Reference($userProviderId))
+            ->replaceArgument(1, new Reference('security.user_checker'))
             ->replaceArgument(2, $id)
         ;
 
